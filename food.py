c472edf9b0688c69cf5116cef3fc4c9be5977289
Aaron Olinger <aaron.olinger@ncf.edu>
2013-09-23 21:53:28 -0400

    Removed C dependencies and extraneous files.

--- food.py
+++ food.py
@@ -1,14 +1,22 @@
+#============IMPORTS==========#
 import vector
 import unittest
-TestCase = unittest.TestCase
 from cells import Cell
+#==========END IMPORTS========#
 
+#Unit Test
+TestCase = unittest.TestCase
+
+#Food Class
 class Food:
+	"""Food items which are eaten by cells"""
 	def __init__(self, x, y):
-		self.energy = 0.5
-		self.pos = vector.Point(x, y)
+		#consider adding randomization factor
+		self.energy = 0.5		#Energy which food will give cell when consumed
+		self.pos = vector.Point(x, y)	#Starting Position
 
 class CreationTest(TestCase):
+	"""UnitTest for food"""
 	def setUp(self):
 		self.food_obj = Food(1, 2)
 	def runTest(self):
